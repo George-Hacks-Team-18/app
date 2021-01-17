13eef0bde88361547534b5fb5f7e32ef46f5fc1c
Ethan Hopkins <58455740+GlowingEmber@users.noreply.github.com>
2021-01-16 16:52:55 -0800

    vaccine pages

--- lib/pages/patient/home_page.dart
+++ lib/pages/patient/home_page.dart
@@ -31,21 +31,28 @@ class HomePage extends StatelessWidget {
                   height: 72,
                   width: MediaQuery.of(context).size.width * 0.4,
                   child: TextButton(
-                    style:
-                        TextButton.styleFrom(backgroundColor: AppTheme.primary),
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (c) => DoseInfoPage(i, dose))),
-                    child: Container(),
-                  ),
+                      style: TextButton.styleFrom(
+                          backgroundColor: AppTheme.primary,
+                          padding: EdgeInsets.zero),
+                      onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (c) => DoseInfoPage(i + 1, dose))),
+                      child: Container(
+                        height: 72,
+                        child: Column(children: [
+                          Image.asset("assets/yesvaccineblank.png"),
+                          ThemedText("Dose ${i + 1}",
+                              color: AppTheme.buttonText)
+                        ]),
+                      )),
                 );
               } else {
                 return Container(
-                  height: 72,
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  color: Color(0xFFFFAC9B),
-                );
+                    height: 72,
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    color: Color(0xFFFFAC9B),
+                    child: Image.asset("assets/novaccineblank.png"));
               }
             },
           ),
