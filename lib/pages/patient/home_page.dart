64919b4365759a0302a0a2a43fb1f2652af290af
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-01-17 11:30:06 -0800

    Revert "in the middle of login"
    This reverts commit 6ae837da5401dd0e6b45a07e3d04aed095ddf81a.

--- lib/pages/patient/home_page.dart
+++ lib/pages/patient/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:app/components/header.dart';
 import 'package:app/components/info_tile.dart';
 import 'package:app/components/section.dart';
@@ -37,6 +35,7 @@ class _HomePageState extends State<HomePage> {
         if (snapshot.connectionState != ConnectionState.done)
           return Center(child: CircularProgressIndicator());
         var data = snapshot.data;
+        print(data);
         int index;
         for (var i = 0; i < data.length; i++) {
           if (data[i]['patientNumber'] == widget.paceintNumber) {
@@ -56,10 +55,7 @@ class _HomePageState extends State<HomePage> {
                 children: List.generate(
                   2,
                   (i) {
-                    var date = jsonDecode(data[index]['date']);
-                    print(date.runtimeType);
-                    if (i == 0 && data[index]['date'] != null) {
-                      print('hi');
+                    if (patient.doses.length > i) {
                       Dose dose = patient.doses[i];
 
                       return ClipRRect(
