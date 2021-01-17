7f4885fb9b848d299f6a6e7bf11c4afbc3e6d6b6
JasonTelanoff <jason.telanoff@gmail.com>
2021-01-16 18:20:07 -0800

    did something

--- lib/pages/doctor/edit_patient_info_page.dart
+++ lib/pages/doctor/edit_patient_info_page.dart
@@ -2,74 +2,84 @@ import 'package:app/components/header.dart';
 import 'package:app/components/info_tile.dart';
 import 'package:app/components/section.dart';
 import 'package:app/components/themed_scaffold.dart';
+import 'package:app/components/themed_text.dart';
 import 'package:app/globals/app_theme.dart';
 import 'package:app/globals/patient_info.dart';
 import 'package:app/models/patient.dart';
+import 'package:app/pages/patient/dose_info_page.dart';
 import 'package:flutter/material.dart';
 
-class EditPatientInfoPage extends StatefulWidget {
-  @override
-  _EditPatientInfoPageState createState() => _EditPatientInfoPageState();
-}
-
-class _EditPatientInfoPageState extends State<EditPatientInfoPage> {
+class EditPatientInfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ThemedScaffold([
       Header(
-        'Edit Patient',
+        'Info',
         showBack: true,
       ),
       SliverList(
           delegate: SliverChildListDelegate([
-        Column(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: List.generate(
             2,
             (i) {
-              if (patient.doses.length > 1 - i) {
-                Dose dose = patient.doses[1 - i];
+              if (patient.doses.length > i) {
+                Dose dose = patient.doses[i];
 
-                return Section(
-                  title: 'Dose ${2 - i}',
-                  children: [
-                    InfoTile(
-                      dose.productName,
-                      'Product Name',
-                      color: AppTheme.secondaryText,
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Container(
+                    height: 100,
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: AppTheme.primary,
+                          padding: EdgeInsets.zero),
+                      onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (c) => DoseInfoPage(i + 1, dose))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/yesvaccineblank.png",
+                            height: 72,
+                          ),
+                          ThemedText(dose.date, color: AppTheme.buttonText)
+                        ],
+                      ),
                     ),
-                    InfoTile(
-                      dose.lotNumber,
-                      'Lot Number',
-                      color: AppTheme.secondaryText,
-                    ),
-                    InfoTile(
-                      dose.date,
-                      'Date',
-                      color: AppTheme.secondaryText,
-                    ),
-                    InfoTile(
-                      dose.professionalOrClinic,
-                      'Professional or Clinic Site',
-                      color: AppTheme.secondaryText,
-                    ),
-                  ],
+                  ),
                 );
               } else {
-                return Section(
-                  title: 'Dose ${2 - i} not yet administered',
-                  color: Color(0xFFFFAC9B),
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Container(
+                    height: 100,
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    color: AppTheme.red,
+                    child: Column(
+                      children: [
+                        Image.asset("assets/novaccineblank.png", height: 72),
+                        ThemedText("Dose ${i + 1}", color: AppTheme.buttonText)
+                      ],
+                    ),
+                  ),
                 );
               }
             },
           ),
         ),
+        SizedBox(height: 16),
         Section(
           title: 'Patient Info',
           color: AppTheme.lightGray,
           children: [
             InfoTile(patient.firstName, 'First Name'),
             InfoTile(patient.lastName, 'Last Name'),
-            InfoTile(patient.middleName, 'Middle Name'),
+            InfoTile(patient.middleName, 'Middle Initial'),
             InfoTile(patient.dateOfBirth, 'Date of Birth'),
             InfoTile(patient.patientNumber, 'Patient Number'),
           ],
