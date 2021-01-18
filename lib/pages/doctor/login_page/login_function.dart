1b7c0241d610f626b966cba415fb33177bccc34c
JasonTelanoff <jason.telanoff@gmail.com>
2021-01-18 00:04:34 -0800

    cloud firestore login

--- lib/pages/doctor/login_page/login_function.dart
+++ lib/pages/doctor/login_page/login_function.dart
@@ -1,4 +1,4 @@
-import 'package:app/models/parse_json.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class DoctorLogin {
@@ -6,24 +6,16 @@ class DoctorLogin {
   DoctorLogin({@required this.username, @required this.password});
 }
 
-Future<bool> login(
-    String username, String password, BuildContext context) async {
-  List<DoctorLogin> patients = [];
-  List json = await getJSONfromFile(context, 'doctor_accounts');
-
-  json.forEach((p) => patients
-      .add(new DoctorLogin(username: p['username'], password: p['password'])));
-
-  bool found = false;
-  patients.forEach((p) {
-    if (p.username == username) {
-      if (p.password == password)
-        found = true;
-      else
-        found = false;
-      return;
-    }
-  });
-
-  return found;
+Future<bool> login(String username, String password) async {
+  await Firestore.instance
+      .collection('doctors')
+      .getDocuments()
+      .then((value) => value.documents.forEach((element) {
+            if (element['username'] == username) if (element['password'] ==
+                password)
+              return true;
+            else
+              return false;
+          }));
+  return false;
 }
